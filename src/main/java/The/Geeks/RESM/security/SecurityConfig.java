639413c5945654c20001be43ace2f25f2f2c42ad
ghramcode18 <81458028+ghramcode18@users.noreply.github.com>
2022-03-10 14:29:27 +0200

    update securityConfig

--- src/main/java/The/Geeks/RESM/security/SecurityConfig.java
+++ src/main/java/The/Geeks/RESM/security/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         // http.authorizeRequests().anyRequest().permitAll();
-        http.authorizeRequests().antMatchers("/api/login/**").permitAll();
+        http.authorizeRequests().antMatchers("/api/login").permitAll();
         http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/user/**").hasAnyAuthority("ROLE_USER");
         http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
         http.authorizeRequests().anyRequest().authenticated();
