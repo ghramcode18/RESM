62c2d291e3bdd2df61f606803293eb68b36ed20c
ghramcode18 <81458028+ghramcode18@users.noreply.github.com>
2022-04-01 20:21:14 +0300

    --make controller for some method

--- src/main/java/The/Geeks/RESM/controller/EstatesController.java
+++ src/main/java/The/Geeks/RESM/controller/EstatesController.java
@@ -5,10 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,9 +29,37 @@ public class EstatesController {
     @Autowired
     EstatesServicesImp estateServiceImp;
 
-    @GetMapping  ("/Estates")
+    @GetMapping  ("/getAllEstate")
     public List<EstatesDto> findAllEstates() {
         return estateServiceImp.getAllEstate();
     }
 
+
+    
+    @PostMapping("/setEstate")
+    public void saveNewEstates(@RequestBody Integer Id, EstatesDto estatesDto) {
+         estateServiceImp.setEstate(Id,estatesDto);
+    }
+
+    @GetMapping("/getEstatesEntityById/{id}")
+    public EstatesEntity getEstatesEntityById(@PathVariable int id) {
+        return estateServiceImp.getEstatesEntityById(id);
+    }
+    @GetMapping("/getEstatesEntityByName/{name}")
+    public EstatesDto getEstatesEntityByName(@PathVariable String name) {
+        return estateServiceImp.searchByName(name);
+    }
+    @PutMapping("/updateEstate")
+    public EstatesDto updateEstate(@RequestBody EstatesDto estatesDto) {
+        return estateServiceImp.updateEstate(estatesDto);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteProduct(@PathVariable int id) {
+         estateServiceImp.deleteEstatesEntityById(id);
+    }
+
+
+
+    
 }
