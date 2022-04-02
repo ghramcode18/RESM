8b4f73caf29da70654d0f91d330e7350a37723aa
ghramcode18 <81458028+ghramcode18@users.noreply.github.com>
2022-04-02 17:31:00 +0300

    --fix problem

--- src/main/java/The/Geeks/RESM/controller/EstatesController.java
+++ src/main/java/The/Geeks/RESM/controller/EstatesController.java
@@ -3,84 +3,53 @@ package The.Geeks.RESM.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import The.Geeks.RESM.dto.EstatesDto;
 import The.Geeks.RESM.entity.EstatesEntity;
 import The.Geeks.RESM.services.EstatesServicesImp;
-import org.springframework.ui.Model;
 
-@Controller
+@RestController
+@RequestMapping(path = { "/", "/api/v1" })
 public class EstatesController {
     @Autowired
     EstatesServicesImp estateServiceImp;
 
-    // display list of EstatesEntity
-    @GetMapping("/")
-    public String viewHomePage(Model model) {
-        return findPaginated(1, "propertyName", "asc", model);
+    @GetMapping("/getAllEstate")
+    public List<EstatesDto> findAllEstates() {
+        return estateServiceImp.getAllEstate();
     }
 
-    @GetMapping("/page/{pageNo}")
-    public String findPaginated(@PathVariable(value = "pageNo") int pageNo,
-            @RequestParam("sortField") String sortField,
-            @RequestParam("sortDir") String sortDir,
-            Model model) {
-        int pageSize = 5;
-
-        Page<EstatesEntity> page = estateServiceImp.findPaginated(pageNo, pageSize, sortField, sortDir);
-        List<EstatesEntity> listEstatesEntity = page.getContent();
-
-        model.addAttribute("currentPage", pageNo);
-        model.addAttribute("totalPages", page.getTotalPages());
-        model.addAttribute("totalItems", page.getTotalElements());
-
-        model.addAttribute("sortField", sortField);
-        model.addAttribute("sortDir", sortDir);
-        model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
-
-        model.addAttribute("listEstatesEntity", listEstatesEntity);
-        return "index";
+    @PostMapping("/setEstate/{Id}")
+    public void saveNewEstates(@PathVariable(name = "Id")  Integer Id, @RequestBody EstatesDto estatesDto) {
+        estateServiceImp.setEstate(Id, estatesDto);
     }
 
-    @GetMapping("/showNewEstatesEntityForm")
-    public String showNewEstatesEntityForm(Model model) {
-        // create model attribute to bind form data
-        EstatesEntity estatesEntity = new EstatesEntity();
-        model.addAttribute("estatesEntity", estatesEntity);
-        return "new_estatesEntity";
+    @GetMapping("/getEstatesEntityById/{id}")
+    public EstatesEntity getEstatesEntityById(@PathVariable int id) {
+        return estateServiceImp.getEstatesEntityById(id);
     }
 
-    @PostMapping("/saveEstatesEntity")
-    public String saveEstatesEntity(@ModelAttribute("estatesEntity") EstatesEntity estatesEntity) {
-        // save estatesEntity to database
-        estateServiceImp.save(estatesEntity);
-        return "redirect:/";
+    @GetMapping("/getEstatesEntityByName/{name}")
+    public EstatesDto getEstatesEntityByName(@PathVariable String name) {
+        return estateServiceImp.searchByName(name);
     }
 
-    @GetMapping("/showFormForUpdate/{id}")
-    public String showFormForUpdate(@PathVariable(value = "id") Integer id, Model model) {
-
-        // get estatesEntity from the service
-        EstatesEntity estatesEntity = estateServiceImp.getEstatesEntityById(id);
-
-        // set estatesEntity as a model attribute to pre-populate the form
-        model.addAttribute("estatesEntity", estatesEntity);
-        return "update_estatesEntity";
+    @PutMapping("/updateEstate")
+    public EstatesDto updateEstate(@RequestBody EstatesDto estatesDto) {
+        return estateServiceImp.updateEstate(estatesDto);
     }
 
-    @GetMapping("/deleteEstatesEntity/{id}")
-    public String deleteEstatesEntity(@PathVariable(value = "id") Integer id) {
-
-        // call delete employee method
-        this.estateServiceImp.deleteEstatesEntityById(id);
-        return "redirect:/";
-
+    @DeleteMapping("/delete/{id}")
+    public void deleteProduct(@PathVariable int id) {
+        estateServiceImp.deleteEstatesEntityById(id);
     }
 
 }
