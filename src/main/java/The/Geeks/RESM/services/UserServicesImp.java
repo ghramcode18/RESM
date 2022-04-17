6004113c4e04f37453490d2f16a6beb4df2e8c23
ghramcode18 <81458028+ghramcode18@users.noreply.github.com>
2022-04-17 22:37:58 +0300

    --

--- src/main/java/The/Geeks/RESM/services/UserServicesImp.java
+++ src/main/java/The/Geeks/RESM/services/UserServicesImp.java
@@ -31,12 +31,11 @@ public class UserServicesImp implements UserService, UserDetailsService {
     private final UserRepo userRepo;
     private final RoleRepo roleRepo;
 
-    private final PasswordEncoder passwordEncoder;    
+    private final PasswordEncoder passwordEncoder;
 
-  
     @Override
     public void addRoleToUser(String username, String roleName) {
-        log.info("Adding role {} to ",roleName ,username);
+        log.info("Adding role {} to ", roleName, username);
         User user = userRepo.findByUsername(username);
         Role role = roleRepo.findByName(roleName);
         user.getRoles().add(role);
@@ -44,7 +43,7 @@ public class UserServicesImp implements UserService, UserDetailsService {
 
     @Override
     public User getUser(String username) {
-        log.info("Fetching user {}",username);
+        log.info("Fetching user {}", username);
         return userRepo.findByUsername(username);
     }
 
@@ -57,14 +56,14 @@ public class UserServicesImp implements UserService, UserDetailsService {
 
     @Override
     public Role saveRole(Role role) {
-        log.info("saving new role{} to the database ",role.getName());
+        log.info("saving new role{} to the database ", role.getName());
 
         return roleRepo.save(role);
     }
 
     @Override
     public User saveUser(User user) {
-        log.info("saving new user{} to the database ",user.getName());
+        log.info("saving new user{} to the database ", user.getName());
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepo.save(user);
     }
@@ -72,22 +71,22 @@ public class UserServicesImp implements UserService, UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepo.findByUsername(username);
-        if (user == null) {log.error("user not found in the database");
+        if (user == null) {
+            log.error("user not found in the database");
             throw new UsernameNotFoundException("user not found in the database");
 
-        } else { log.info("user found in the database :{}",username);
-    
-    
-               }
-            Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
-            user.getRoles().forEach(role -> 
-            {authorities.add(new SimpleGrantedAuthority(role.getName()));
-            });
-            return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
-                    authorities);
+        } else {
+            log.info("user found in the database :{}", username);
+
         }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        });
+        log.info(user.getName() +"&&   "+ user.getPassword());
 
-
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                authorities);
     }
 
-
+}
