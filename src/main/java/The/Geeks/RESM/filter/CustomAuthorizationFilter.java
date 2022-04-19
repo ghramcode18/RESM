033da1ef3909b8206581ba03cce6c617154fd3d6
ghramcode18 <81458028+ghramcode18@users.noreply.github.com>
2022-04-19 22:39:27 +0300

    change role

--- src/main/java/The/Geeks/RESM/filter/CustomAuthorizationFilter.java
+++ src/main/java/The/Geeks/RESM/filter/CustomAuthorizationFilter.java
@@ -42,7 +42,8 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        if (request.getServletPath().equals("/api/login")||request.getServletPath().equals("/api/token/refresh") ||request.getServletPath().equals("/api/users")) {
+        if (request.getServletPath().equals("/api/login")
+        ||request.getServletPath().equals("/api/token/refresh")) {
             filterChain.doFilter(request, response);
 
         } else {
